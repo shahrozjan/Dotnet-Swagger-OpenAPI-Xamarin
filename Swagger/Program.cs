01839aef1a8604d6fd2220e5abe1ddfad77b36e9
shahrozjan <shahrozjan67@gmail.com>
2023-04-13 16:58:24 -0400

    Alright, we are done

--- Swagger/Program.cs
+++ Swagger/Program.cs
@@ -1,11 +1,32 @@
-﻿var builder = WebApplication.CreateBuilder(args);
+﻿using Microsoft.OpenApi.Models;
+var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(options =>
+{
+    options.SwaggerDoc("v1", new OpenApiInfo
+    {
+        Version = "v1",
+        Title = "ToDo API",
+        Description = "An ASP.NET Core Web API for managing ToDo items",
+        TermsOfService = new Uri("https://example.com/terms"),
+        Contact = new OpenApiContact
+        {
+            Name = "Example Contact",
+            Url = new Uri("https://example.com/contact")
+        },
+        License = new OpenApiLicense
+        {
+            Name = "Example License",
+            Url = new Uri("https://example.com/license")
+        }
+    });
+});
+
 
 var app = builder.Build();
 
@@ -13,10 +34,17 @@ var app = builder.Build();
 if (app.Environment.IsDevelopment())
 {
     //comment
+    //second
     //this is a change
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+
+app.UseSwaggerUI(options =>
+{
+    options.SwaggerEndpoint("/swagger/v1/swagger.json", "v1");
+    options.RoutePrefix = string.Empty;
+});
 
 app.UseHttpsRedirection();
 
--- Swagger/obj/Debug/net7.0/Swagger.AssemblyInfo.cs
+++ Swagger/obj/Debug/net7.0/Swagger.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Swagger")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+380f2cf1247733787d662af594f0d6f69cce41ff")]
 [assembly: System.Reflection.AssemblyProductAttribute("Swagger")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Swagger")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
